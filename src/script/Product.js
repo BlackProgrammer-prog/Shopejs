3c96ad7501433f58c88751be4fab68436502bef8
BlackProgrammer-prog <aziziparham2020@gmail.com>
2024-11-28 12:55:01 +0330

    Home Site

--- src/script/Product.js
+++ src/script/Product.js
@@ -1,30 +1,47 @@
 // import {l} from "vite/dist/node/types.d-aGj9QkWt.js";
 export function product(productId){
-    // let ProductPhoto = document.getElementById("ProductPhoto");
-    // let ProductName = document.getElementById("ProductName");
-    // let ProductDescription = document.getElementById("ProductDescription");
-    // let Size1 = document.getElementById("Size1");
-    // let Size2 = document.getElementById("Size2");
-    // let Size3 = document.getElementById("Size3");
-    // let Color1 = document.getElementById("Color1");
-    // let Color2 = document.getElementById("Color2");
-    // let Color3 = document.getElementById("Color3");
-    // let Color4 = document.getElementById("Color4");
-    // let ProductPrice = document.getElementById("ProductPrice");
+    let ProductObject;
+    let ProductPhoto = document.getElementById("ProductPhoto");
+    let ProductName = document.getElementById("ProductName");
+    let ProductDescription = document.getElementById("ProductDescription");
+    let Size1 = document.getElementById("Size1");
+    let Size2 = document.getElementById("Size2");
+    let Size3 = document.getElementById("Size3");
+    let Color1 = document.getElementById("Color1");
+    let Color2 = document.getElementById("Color2");
+    let Color3 = document.getElementById("Color3");
+    let Color4 = document.getElementById("Color4");
+    let ProductPrice = document.getElementById("ProductPrice");
     let Url = `http://localhost:3000/Products/${productId}`;
-    fetch(Url).then((response)=>{
-        if (!response.ok){
-            throw new Error("محصول یافت نشد");
+    async function fetchProduct(){
+        try {
+            const response = await fetch(Url);
+            if (!response.ok){
+                 console.error(Error(`محصول یافت نشد:${response.status}`))
+            }
+            const Product = await response.json();
+            console.log("اطلاعات محصول",Product);
+            ProductObject = Product;
+
+        }catch (error){
+            console.log("خطا در دریافت محصول",error);
+            throw error
         }
-        return response.json();
-    })
-        .then((product)=>{
-            console.log('اطلاعات محصول', product);
-        })
-        .catch((error)=>{
-            console.log("خطا در دریافت محصول:", error);
-    });
-
-
-
+    }
+    async function main(){
+        await fetchProduct();
+        console.log(ProductObject.images);
+        ProductPhoto.src = ProductObject.images;
+        ProductName.innerText = `کفش:${ProductObject.title}`;
+        ProductPrice.innerText = `قیمت:${ProductObject.price}$`;
+        Size1.innerText = ProductObject.size[0];
+        Size2.innerText = ProductObject.size[1];
+        Size3.innerText = ProductObject.size[2];
+        Color1.innerText = ProductObject.color[0];
+        Color2.innerText = ProductObject.color[1];
+        Color3.innerText = ProductObject.color[2];
+        Color4.innerText = ProductObject.color[3];
+        ProductDescription.innerText = "این کفش بهترین کفش بازار با راحتی بی نظیر است";
+    }
+    main();
 }
