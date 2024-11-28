01726f4fc6ff9de85b92a4e1789413b4f936b792
BlackProgrammer-prog <aziziparham2020@gmail.com>
2024-11-28 16:42:19 +0330

    Home Site

---
+++ src/script/UserClick.js
@@ -0,0 +1,403 @@
+let DivNum;
+import {product} from "./Product.js";
+export function UserClick(){
+const Product1 = document.getElementById("product1");
+const Product2 = document.getElementById("product2");
+const Product3 = document.getElementById("product3");
+const Product4 = document.getElementById("product4");
+const Product5 = document.getElementById("product5");
+const Product6 = document.getElementById("product6");
+const Product7 = document.getElementById("product7");
+const Product8 = document.getElementById("product8");
+const Product9 = document.getElementById("product9");
+const Product10 = document.getElementById("product10");
+const Product11 = document.getElementById("product11");
+const Product12 = document.getElementById("product12");
+const Product13 = document.getElementById("product13");
+const Product14 = document.getElementById("product14");
+const Product15 = document.getElementById("product15");
+const Product16 = document.getElementById("product16");
+const Product17 = document.getElementById("product17");
+const Product18 = document.getElementById("product18");
+const Product19 = document.getElementById("product19");
+const Product20 = document.getElementById("product20");
+const Product21 = document.getElementById("product21");
+const Product22 = document.getElementById("product22");
+const Product23 = document.getElementById("product23");
+const Product24 = document.getElementById("product24");
+const Product25 = document.getElementById("product25");
+const Product26 = document.getElementById("product26");
+const Product27 = document.getElementById("product27");
+const Product28 = document.getElementById("product28");
+const Product29 = document.getElementById("product29");
+const Product30 = document.getElementById("product30");
+const Product31 = document.getElementById("product31");
+const Product32 = document.getElementById("product32");
+const Product33 = document.getElementById("product33");
+const Product34 = document.getElementById("product34");
+const Product35 = document.getElementById("product35");
+const Product36 = document.getElementById("product36");
+const Product37 = document.getElementById("product37");
+const Product38 = document.getElementById("product38");
+const Product39 = document.getElementById("product39");
+const Product40 = document.getElementById("product40");
+const Product41 = document.getElementById("product41");
+const Product42 = document.getElementById("product42");
+const Product43 = document.getElementById("product43");
+const Product44 = document.getElementById("product44");
+const Product45 = document.getElementById("product45");
+const Product46 = document.getElementById("product46");
+const Product47 = document.getElementById("product47");
+const Product48 = document.getElementById("product48");
+const Product49 = document.getElementById("product49");
+const Product50 = document.getElementById("product50");
+const Product51 = document.getElementById("product51");
+const Product52 = document.getElementById("product52");
+const Product53 = document.getElementById("product53");
+const Product54 = document.getElementById("product54");
+const Product55 = document.getElementById("product55");
+const Product56 = document.getElementById("product56");
+const DomHomeSite = document.getElementById("DomHomeSite");
+const DomProduct = document.getElementById("DomProduct");
+const ButtonBack = document.getElementById("back");
+Product1.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=1;
+    product(DivNum);
+});
+Product2.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=2;
+    product(DivNum);
+});
+Product3.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=3;
+    product(DivNum);
+});
+Product4.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=4;
+    product(DivNum);
+});
+Product5.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=5;
+    product(DivNum);
+});
+Product6.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=6;
+    product(DivNum);
+});
+Product7.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=7;
+    product(DivNum);
+});
+Product8.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=8;
+    product(DivNum);
+});
+Product9.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=9;
+    product(DivNum);
+});
+Product10.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=10;
+    product(DivNum);
+});
+Product11.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=11;
+    product(DivNum);
+});
+Product12.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=12;
+    product(DivNum);
+});
+Product13.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=13;
+    product(DivNum);
+});
+Product14.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=14;
+    product(DivNum);
+});
+Product15.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=15;
+    product(DivNum);
+});
+Product16.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=16;
+    product(DivNum);
+});
+Product17.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=17;
+    product(DivNum);
+});
+Product18.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=18;
+    product(DivNum);
+});
+Product19.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=19;
+    product(DivNum);
+});
+Product20.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=20;
+    product(DivNum);
+});
+Product21.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=21;
+    product(DivNum);
+});
+Product22.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=22;
+    product(DivNum);
+});
+Product23.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=23;
+    product(DivNum);
+});
+Product24.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=24;
+    product(DivNum);
+});
+Product25.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=25;
+    product(DivNum);
+});
+Product26.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=26;
+    product(DivNum);
+});
+Product27.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=27;
+    product(DivNum);
+});
+Product28.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=28;
+    product(DivNum);
+});
+Product29.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=29;
+    product(DivNum);
+});
+Product30.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=30;
+    product(DivNum);
+});
+Product31.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=31;
+    product(DivNum);
+});
+Product32.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=32;
+    product(DivNum);
+});
+Product33.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=33;
+    product(DivNum);
+});
+Product34.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=34;
+    product(DivNum);
+});
+Product35.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=35;
+    product(DivNum);
+});
+Product36.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=36;
+    product(DivNum);
+});
+Product37.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=37;
+    product(DivNum);
+});
+Product38.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=38;
+    product(DivNum);
+});
+Product39.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=39;
+    product(DivNum);
+});
+Product40.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=40;
+    product(DivNum);
+});
+Product41.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=41;
+    product(DivNum);
+});
+Product42.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=42;
+    product(DivNum);
+});
+Product43.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=43;
+    product(DivNum);
+});
+Product44.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=44;
+    product(DivNum);
+});
+Product45.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=45;
+    product(DivNum);
+});
+Product46.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=46;
+    product(DivNum);
+});
+Product47.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=47;
+    product(DivNum);
+});
+Product48.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=48;
+    product(DivNum);
+});
+Product49.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=49;
+    product(DivNum);
+});
+Product50.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=50;
+    product(DivNum);
+});
+Product51.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=51;
+    product(DivNum);
+});
+Product52.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=52;
+    product(DivNum);
+});
+Product53.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=53;
+    product(DivNum);
+});
+Product54.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=54;
+    product(DivNum);
+});
+Product55.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=55;
+    product(DivNum);
+});
+Product56.addEventListener("click",()=>{
+    DomHomeSite.style.display = "none";
+    DomProduct.style.display = "block";
+    DivNum=56;
+    product(DivNum);
+});
+ButtonBack.addEventListener("click",()=>{
+    DomHomeSite.style.display = "block";
+    DomProduct.style.display = "none";
+})
+}
